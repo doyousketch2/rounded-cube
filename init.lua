0cd8233f2d1fed65f0d055d66c9f76ffb9fc0a51
doyousketch2 <doyousketch2@yahoo.com>
2018-02-17 13:28:48 -0500

    Add files via upload

---
+++ init.lua
@@ -0,0 +1,26 @@
+-- rounded cubes
+
+minetest .register_node( 'rounded_cubes:rounded_cube',
+  {
+    drop  = 'rounded_cubes:rounded_cube',
+    description = 'Rounded Cube',
+    drawtype = 'mesh',
+
+    mesh = 'rounded_cube.obj',
+    tiles = { 'rounded_cube.png' },
+    inventory_image = minetest .inventorycube('rounded_cube.png'),
+
+    light_source = 4,
+    paramtype = 'light',
+    groups = {snappy=1, choppy=2, oddly_breakable_by_hand=2},
+
+    selection_box = {
+      type = 'fixed',
+      fixed = {
+        {-0.495, -0.495, -0.495,  0.495, 0.495, 0.495 },
+      }, -- fixed
+    }, -- selection_box
+
+  } -- rounded_cube
+) -- register_node
+
