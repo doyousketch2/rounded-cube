276aebd4b1503136e506fa9e0061c48497ab135d
doyousketch2 <doyousketch2@yahoo.com>
2018-02-17 15:40:09 -0500

    Add files via upload

--- init.lua
+++ init.lua
@@ -4,20 +4,57 @@ minetest .register_node( 'rounded_cubes:rounded_cube',
   {
     drop  = 'rounded_cubes:rounded_cube',
     description = 'Rounded Cube',
+
     drawtype = 'mesh',
-
     mesh = 'rounded_cube.obj',
-    tiles = { 'rounded_cube.png' },
-    inventory_image = minetest .inventorycube('rounded_cube.png'),
 
-    light_source = 4,
+    tiles = { 'rounded_cube.png' },
+
+    light_source = 1,
     paramtype = 'light',
+
     groups = {snappy=1, choppy=2, oddly_breakable_by_hand=2},
+    inventory_image = minetest .inventorycube('rounded_cube.png'),
 
     selection_box = {
       type = 'fixed',
       fixed = {
         {-0.475, -0.475, -0.475,  0.475, 0.475, 0.475 },
+      }, -- fixed
+    }, -- selection_box
+
+  } -- rounded_cube
+) -- register_node
+
+--~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+minetest .register_node( 'rounded_cubes:cylindrical_cube',
+  {
+    drop  = 'rounded_cubes:cylindrical_cube',
+    description = 'Cylindrical Cube',
+
+    drawtype = 'mesh',
+    mesh = 'cylindrical_cube.obj',
+
+    tiles  = {
+      'rounded_cube.png', -- top
+      'rounded_cube.png', -- bottom
+      'rounded_cube.png', -- left
+      'rounded_cube.png', -- right
+      'rounded_cube.png', -- back
+      'rounded_cube.png', -- front
+    }, -- tiles
+
+    light_source = 1,
+    paramtype = 'light',
+
+    groups = {snappy=1, choppy=2, oddly_breakable_by_hand=2},
+    inventory_image = minetest .inventorycube('cylindrical_cube.png', 'rounded_cube.png', 'rounded_cube.png'),
+
+    selection_box = {
+      type = 'fixed',
+      fixed = {
+        {-0.475, -0.5, -0.475,  0.475, 0.5, 0.475 },
       }, -- fixed
     }, -- selection_box
 
